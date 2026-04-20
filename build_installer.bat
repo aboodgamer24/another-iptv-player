8b585958fecb4e722562a0c8d4f4333af1bc301e
aboodgamer24 <abdualrahmanothman24@gmail.com>
2026-04-20 21:32:20 +0200

    chore: use call command to execute flutter build in installer script

--- build_installer.bat
+++ build_installer.bat
@@ -1,6 +1,6 @@
 @echo off
 echo Building Flutter Windows release...
-flutter build windows --release
+call flutter build windows --release
 echo Compiling Inno Setup installer...
 "C:\Program Files (x86)\Inno Setup 6\ISCC.exe" installer.iss
 echo Done! Installer is in release_bundle\C4-TV_Setup.exe
